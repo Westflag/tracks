589b504effcb635d7002b88d4fcc723baf401897
Andrii Brelyk <andrii.brelyk@lobyco.com>
2025-04-24 20:23:49 +0300

    Build tracks

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import 'modern-normalize'
+
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {PersistGate} from 'redux-persist/integration/react'
+
+import App from './components/App.jsx'
+import {persistor, store} from './redux/store.js'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <App/>
+                </PersistGate>
+            </Provider>
+        </BrowserRouter>
+    </StrictMode>
 )
